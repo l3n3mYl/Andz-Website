1795cec6a4280392695ce4bbc75318cff2ed712c
l3n3mYl <asavkinas123@gmail.com>
2021-11-12 12:58:16 +0000

    finished index page

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,46 @@
-const Footer = ({ children, title }) => {
+import styles from '../styles/css/footer.module.css'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTwitter, faSnapchatGhost } from '@fortawesome/free-brands-svg-icons'
 
+const Footer = ({ author }) => {
+
+    const facebook = (e) => {
+        e.preventDefault()
+        window.location.assign(author[0].facebook)
+    }
+
+    const instagram = (e) => {
+        e.preventDefault()
+        window.location.assign(author[0].instagram)
+    }
+
+    const twitter = (e) => {
+        e.preventDefault()
+        window.location.assign(author[0].twitter)
+    }
+
+    const snapchat = (e) => {
+        e.preventDefault()
+        window.location.assign(author[0].snapchat)
+    }
 
     return (
-        <>
-            
-        </>
+        <div className={styles.mainDiv}>
+            <div className={styles.socMedia}>
+                <h3>Follow {author[0].hashtag}</h3>
+                <div className={styles.links}>
+                    <i onClick={facebook}><FontAwesomeIcon icon={faFacebook}/></i>
+                    <i onClick={instagram}><FontAwesomeIcon icon={faInstagram}/></i>
+                    <i onClick={twitter}><FontAwesomeIcon icon={faTwitter}/></i>
+                    <i onClick={snapchat}><FontAwesomeIcon icon={faSnapchatGhost}/></i>
+                </div>
+            </div>
+            <div className={styles.info}>
+                <p>{author[0].street}, <br />{author[0].city}, <br />{author[0].country}</p>
+                <p>+{author[0].phone}</p>
+            </div>
+        </div>
     )
 }
 
--- components/Toolbar.js
+++ components/Toolbar.js
@@ -6,13 +6,13 @@ const Toolbar = ({ title }) => {
 
     return (
         <header className={styles.header}>
-            <h1 className={styles.smallLogo}><Image src='/logo.png' width={100} height={100} /></h1>
+            <h1 className={styles.smallLogo}><Image src='/logo.png' width={75} height={60} /></h1>
             <input type="checkbox" id={styles.menuToggle} className={styles.menuToggle} />
             <nav>
                 <ul>
                     <li>{title=='Galerija' ? <Link href='/galerija' className={styles.highlight}><a>Galerija</a></Link> : <Link href='/galerija' className={styles.normal}><a>Galerija</a></Link>}</li>
                     <li>{title=='Paslaugos' ? <Link href='/paslaugos' className={styles.highlight}><a>Paslaugos</a></Link> : <Link href='/paslaugos' className={styles.normal}><a>Paslaugos</a></Link>}</li>
-                    <i className={styles.bigLogo}><Image src='/logo.png' width={100} height={100} /></i>
+                    <i className={styles.bigLogo}><Image src='/logo1.png' width={40} height={33} /></i>
                     <li>{title=='Apie' ? <Link href='/apie' className={styles.highlight}><a>Apie</a></Link> : <Link href='/apie' className={styles.normal}><a>Apie</a></Link>}</li>
                     <li>{title=='Kontaktai' ? <Link href='/kontaktai' className={styles.highlight}><a>Kontaktai</a></Link> : <Link href='/kontaktai' className={styles.normal}><a>Kontaktai</a></Link>}</li>
                 </ul>
