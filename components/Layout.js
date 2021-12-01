b1a599d882cf83159ffd266d5360ffb275c8b2a1
l3n3mYl <asavkinas123@gmail.com>
2021-12-01 12:13:34 +0000

    Scrollbar visual update

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,18 @@
 import Head from 'next/head'
 import Footer from './Footer'
 import Toolbar from './Toolbar'
-import Scrollbar from './Scrollbar'
 
 const Layout = ({ children, title, author }) => {
 
     return (
-        <>
-            <Scrollbar />
-            <Head>
-                <title> Andzelika JPG | {title}</title>
-            </Head>
-            <Toolbar title={title} />
-            {children}
-            <Footer author={author} />
-        </>
+      <>
+        <Head>
+          <title> Andzelika JPG | {title}</title>
+        </Head>
+        <Toolbar title={title} />
+          {children}
+        <Footer author={author} />
+      </>
     )
 }
 
