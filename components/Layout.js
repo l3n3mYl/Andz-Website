2a6f085858ca352ca92992c93f4811d1c7426cf6
l3n3mYl <asavkinas123@gmail.com>
2021-12-09 14:17:38 +0000

    Added meta, adjusted el visability

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,9 @@ const Layout = ({ children, title, author }) => {
     return (
       <>
         <Head>
+          <meta name="description" content="Photography Portfolio" />
+          <meta name="author" content={author.name} />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title> Andzelika JPG | {title}</title>
           <link rel="shortcut icon" href="icon.ico" />
         </Head>
